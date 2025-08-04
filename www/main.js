09c289e891689380c3d63000b5447c2fcda679ea
HemangajitDutta <hemangodutta7@gmail.com>
2025-08-04 22:21:16 +0530

    Hood design done

---
+++ www/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+    $('.text').texllate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: {
+            effect: "bounceOut",
+        },
+
+    });
+});
